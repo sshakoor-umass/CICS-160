787cc965be01577c7d92f1fbafa0bc0bf4be573d
sshakoor-umass <sshakoor@umass.edu>
2026-02-02 15:58:03 -0500

    d

--- project1.py
+++ project1.py
@@ -1,4 +1,5 @@
 #Sheik Shakoor
+
 def is_prime(n):
     if n <= 1:
         return False
@@ -8,3 +9,26 @@ def is_prime(n):
             return False
 
     return True
+def are_relatively_prime(x, y):
+    smallest = min(x, y)
+
+    for i in range(2, smallest + 1):
+        if x % i == 0 and y % i == 0:
+            return False
+
+    return True
+def primes_up_to(n):
+    primes = []
+
+    for i in range(2, n):
+        if is_prime(i):
+            primes.append(i)
+
+    return primes
+
+print(is_prime(10))  
+print(is_prime(17))  
+print(are_relatively_prime(10, 17))  
+print(are_relatively_prime(21, 10))  
+print(primes_up_to(10))  
+print(primes_up_to(1))  
